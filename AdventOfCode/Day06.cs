581497c99792dc8c10a65827e864121c4e79570e
JSLandbo <jacob@landbo.dk>
2024-12-17 22:44:37 +0100

    heavy performance improvement. 3D bool array

--- AdventOfCode/Day06.cs
+++ AdventOfCode/Day06.cs
@@ -14,11 +14,12 @@ public sealed class Day06 : BaseDay
     public override ValueTask<string> Solve_1()
     {
         var result = 0;
-        var map = new char[_input.Length, _input[0].Length, 2];
         var currentPosition = (X: 0, Y: 0);
-        for (var i = 0; i < _input.Length; i++)
-        for (var j = 0; j < _input[i].Length; j++)
-            if ((map[i, j, 0] = _input[i][j]) == '^') currentPosition = (j, i);
+        var map = new char[_input.Length, _input[0].Length, 2]; // Map of the area
+        // Fill map and find starting position
+        for (var i = 0; i < _input.Length; i++) // i is the row (Y)
+            for (var j = 0; j < _input[i].Length; j++) // j is the column (X)
+                if ((map[i, j, 0] = _input[i][j]) == '^') currentPosition = (X: j, Y: i); 
         var direction = (X: 0, Y: -1);
         while (true)
         {   // Loop until trailing off map, and turn right if there's a hindrance
@@ -31,7 +32,14 @@ public sealed class Day06 : BaseDay
             }
             if (map[nextPosition.Y, nextPosition.X, 0] == '#')
             {   // Oops, hindrance hit! Turn right... 
-                direction = RotateRight(direction);
+                direction = direction switch
+                {
+                    (0, -1) => (1, 0), // Up -> Right
+                    (1, 0) => (0, 1), // Right -> Down
+                    (0, 1) => (-1, 0), // Down -> Left
+                    (-1, 0) => (0, -1), // Left -> Up
+                    _ => direction
+                };
                 continue;
             }
             currentPosition = nextPosition;
@@ -42,10 +50,17 @@ public sealed class Day06 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
+        var start = (X: 0, Y: 0); // Starting position        
+        var map = new char[_input.Length, _input[0].Length]; // Map of the area
+        // Fill map and find starting position
+        for (var i = 0; i < _input.Length; i++) // i is the row (Y)
+            for (var j = 0; j < _input[i].Length; j++) // j is the column (X)
+                if ((map[i, j] = _input[i][j]) == '^') start = (X: j, Y: i); 
+        // Width * Height: Check all possible blockades
         var infiniteLoops = 0;
-        for (var i = 0; i < _input.Length; i++) // Width * Height: Check all possible blockades
-            for (var j = 0; j < _input[i].Length; j++) 
-                infiniteLoops += DoesBlockadeCauseInfiniteLoop(_input, (i, j));
+        for (var i = 0; i < _input.Length; i++) // i is the row (Y)
+            for (var j = 0; j < _input[i].Length; j++) // j is the column (X)
+                infiniteLoops += DoesBlockadeCauseInfiniteLoop(map, start, (X: j, Y: i)); // Pass X as j and Y as i
         
         return new ValueTask<string>($"{infiniteLoops}");
     }
@@ -58,43 +73,35 @@ public sealed class Day06 : BaseDay
     
     private static int GetCountOfUniqueTilesSteppedOn(char[,,] map) => Enumerable.Range(0, map.GetLength(0)).SelectMany(i => Enumerable.Range(0, map.GetLength(1)).Where(j => map[i, j, 1] == 'X')).Count();
     
-    private static (int X, int Y) RotateRight((int X, int Y) direction) => direction switch
+    private static int DoesBlockadeCauseInfiniteLoop(char[,] map, (int X, int Y) position, (int X,int Y) blockade)
     {
-        (0, -1) => (1, 0), // Up -> Right
-        (1, 0) => (0, 1), // Right -> Down
-        (0, 1) => (-1, 0), // Down -> Left
-        (-1, 0) => (0, -1), // Left -> Up
-        _ => direction
-    };
-    
-    private static int DoesBlockadeCauseInfiniteLoop(string[] file, (int X,int Y) blockade)
-    {
-        if (file[blockade.X][blockade.Y] != '.') return 0; // Blockade must be on empty space
-        var map = new char[file.Length, file[0].Length]; // Map of the area
-        var currentPosition = (X:0, Y:0);
-        for (var i = 0; i < file.Length; i++) // Fill map and find starting position
-        for (var j = 0; j < file[i].Length; j++) 
-            if ((map[i, j] = file[i][j]) == '^') currentPosition = (X:j, Y:i);
-        var steps = new Dictionary<(int X, int Y), List<(int X, int Y)>>(); // Steps taken
+        if (map[blockade.Y, blockade.X] != '.') return 0; // Blockade must be on empty space
+        var mapWidth = map.GetLength(1); // Cache map dimensions
+        var mapHeight = map.GetLength(0);
+        var visited = new bool[mapHeight, mapWidth, 4];
         var direction = (X:0, Y:-1); // Move up initially
-        map[blockade.X, blockade.Y] = '#'; // Insert blockade into map
+        var directionIndex = 0; 
         while (true)
-        {
-            if (steps.TryGetValue(currentPosition, out var step))
-            {   // If we've been here before with the same direction then it's an infinite loop.
-                if (step.Any(dir => dir == direction)) return 1;
-                step.Add(direction); // Add direction to steps. 
-            }
-            else steps[currentPosition] = [direction]; // First time here, add direction.
-            var nextPosition = (X:currentPosition.X + direction.X, Y:currentPosition.Y + direction.Y);
+        {   // If we've been here before with the same direction then it's an infinite loop.
+            if (visited[position.Y, position.X, directionIndex]) return 1;
+            visited[position.Y, position.X, directionIndex] = true; 
+            var nextPosition = (X:position.X + direction.X, Y:position.Y + direction.Y);
             if (TrailingOffMap2D(map, nextPosition)) return 0; // Trailing off map, no infinite loop.
-            if (map[nextPosition.Y, nextPosition.X] == '#')
+            var nextTile = map[nextPosition.Y, nextPosition.X];
+            if (nextTile == '#' || nextPosition == blockade)
             {   // We will hit a blockade if we continue, turn right.
-                direction = RotateRight(direction);
+                directionIndex = (directionIndex + 1) % 4;
+                direction = directionIndex switch
+                {   // Rotate direction clockwise (up -> right -> down -> left)
+                    0 => (X: 0, Y: -1),
+                    1 => (X: 1, Y: 0),
+                    2 => (X: 0, Y: 1),
+                    3 => (X: -1, Y: 0),
+                    _ => direction
+                };
                 continue;
             }
-            // Move to next position
-            currentPosition = nextPosition;
+            position = nextPosition;
         }
     }
 }
