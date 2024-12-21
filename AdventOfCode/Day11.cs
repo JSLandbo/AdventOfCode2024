2509164bf48b7760adb7f52607c0372c776a1f36
JSLandbo <jacob@landbo.dk>
2024-12-21 20:30:29 +0100

    Bongus Chongus 11

--- AdventOfCode/Day11.cs
+++ AdventOfCode/Day11.cs
@@ -1,3 +1,6 @@
+using System.Numerics;
+using System.Text;
+
 namespace AdventOfCode;
 
 public sealed class Day11 : BaseDay
@@ -11,11 +14,50 @@ public sealed class Day11 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        return default;
+        var numbers = _input.Split(' ').Select(BigInteger.Parse).ToList();
+        for (var i = 0; i < 25; i++)
+            Blink(numbers);
+        return new ValueTask<string>($"{numbers.Count}");
     }
 
     public override ValueTask<string> Solve_2()
     {
-        return default;
+        var numbers = _input.Split(' ').Select(BigInteger.Parse).ToList();
+        for (var i = 0; i < 75; i++)
+            Blink(numbers);
+        return new ValueTask<string>($"{numbers.Count}");
     }
+    
+    private static void Blink(List<BigInteger> input)
+    {
+        var result = new List<BigInteger>();
+        foreach (var number in input)
+        {
+            RunThroughNumber(result, number);
+        };
+        input.Clear();
+        input.AddRange(result);
+    }
+    
+    private static void RunThroughNumber(List<BigInteger> result, BigInteger number)
+    {
+        if (number == 0)
+        {
+            result.Add(1);
+            return;
+        }
+        
+        var length = (long)BigInteger.Log10(number) + 1;
+        if (length % 2 == 0)
+        {
+            var half = (int)length / 2;
+            result.Add((long)(number / BigInteger.Pow(10, half)));
+            result.Add((long)(number % BigInteger.Pow(10, half)));
+        }
+        else
+        {
+            result.Add(number * 2024);
+        }
+    }
+
 }
