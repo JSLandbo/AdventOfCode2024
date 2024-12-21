a42d8e5de25c0ff4e49b69c39c37707aa779ce44
JSLandbo <jacob@landbo.dk>
2024-12-21 23:15:47 +0100

    Chronos Dongus Omegus Completus 11

--- AdventOfCode/Day11.cs
+++ AdventOfCode/Day11.cs
@@ -1,5 +1,4 @@
 using System.Numerics;
-using System.Text;
 
 namespace AdventOfCode;
 
@@ -14,50 +13,48 @@ public sealed class Day11 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        var numbers = _input.Split(' ').Select(BigInteger.Parse).ToList();
-        for (var i = 0; i < 25; i++)
-            Blink(numbers);
-        return new ValueTask<string>($"{numbers.Count}");
+        var numbers = _input.Split(' ').Select(BigInteger.Parse).ToArray();
+        var result = RunThroughNumbers(numbers, 25);
+        return new ValueTask<string>($"{result}");
+    }
+
+    private static BigInteger RunThroughNumbers(BigInteger[] numbers, int steps)
+    {
+        BigInteger sum = 0;
+        foreach (var number in numbers)
+        {
+            sum += RecursivelyRunThroughNumber(number, steps);
+        }
+        return sum;
+    }
+
+    private static readonly Dictionary<(BigInteger number, int steps), BigInteger> B4 = [];
+
+    private static BigInteger RecursivelyRunThroughNumber(BigInteger number, int steps)
+    {
+        if (steps == 0) return 1;
+        if (B4.ContainsKey((number, steps))) return B4[(number, steps)];
+        BigInteger sum = 0;
+        if (number == 0) sum += RecursivelyRunThroughNumber(1, steps - 1);
+        else
+        {
+            var length = (int)BigInteger.Log10(number) + 1;
+            if (length % 2 != 0) sum += RecursivelyRunThroughNumber(number * 2024, steps - 1);
+            else
+            {
+                var half = length / 2;
+                sum += RecursivelyRunThroughNumber(number / BigInteger.Pow(10, half), steps - 1) +
+                       RecursivelyRunThroughNumber(number % BigInteger.Pow(10, half), steps - 1);
+            }
+        }
+        B4[(number, steps)] = sum;
+        return sum;
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var numbers = _input.Split(' ').Select(BigInteger.Parse).ToList();
-        for (var i = 0; i < 75; i++)
-            Blink(numbers);
-        return new ValueTask<string>($"{numbers.Count}");
+        var numbers = _input.Split(' ').Select(BigInteger.Parse).ToArray();
+        var result = RunThroughNumbers(numbers, 75);
+        return new ValueTask<string>($"{result}");
     }
-    
-    private static void Blink(List<BigInteger> input)
-    {
-        var result = new List<BigInteger>();
-        foreach (var number in input)
-        {
-            RunThroughNumber(result, number);
-        };
-        input.Clear();
-        input.AddRange(result);
-    }
-    
-    private static void RunThroughNumber(List<BigInteger> result, BigInteger number)
-    {
-        if (number == 0)
-        {
-            result.Add(1);
-            return;
-        }
-        
-        var length = (long)BigInteger.Log10(number) + 1;
-        if (length % 2 == 0)
-        {
-            var half = (int)length / 2;
-            result.Add((long)(number / BigInteger.Pow(10, half)));
-            result.Add((long)(number % BigInteger.Pow(10, half)));
-        }
-        else
-        {
-            result.Add(number * 2024);
-        }
-    }
-
 }
