44edd2185836121607c3ecfab6422d8e26589586
JSLandbo <jacob@landbo.dk>
2024-12-22 11:35:37 +0100

    input, map

--- AdventOfCode/Day08.cs
+++ AdventOfCode/Day08.cs
@@ -1,6 +1,25 @@
 namespace AdventOfCode;
 
-public class Day08
+public sealed class Day08 : BaseDay
 {
-    
+    private readonly string[] _input;
+
+    public Day08()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var map = _input.Select(e => e.ToArray()).ToArray();
+        
+        return default;
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var map = _input.Select(e => e.ToArray()).ToArray();
+        
+        return default;
+    }
 }
