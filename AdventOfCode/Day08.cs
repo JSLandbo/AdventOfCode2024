1b2637be40a2b9e4bcf7c78ea51b5f1509400d12
JSLandbo <jacob@landbo.dk>
2024-12-22 12:19:50 +0100

    Get nodes

--- AdventOfCode/Day08.cs
+++ AdventOfCode/Day08.cs
@@ -12,6 +12,7 @@ public sealed class Day08 : BaseDay
     public override ValueTask<string> Solve_1()
     {
         var map = _input.Select(e => e.ToArray()).ToArray();
+        var nodes = GetNodes(map);
         
         return default;
     }
@@ -19,7 +20,24 @@ public sealed class Day08 : BaseDay
     public override ValueTask<string> Solve_2()
     {
         var map = _input.Select(e => e.ToArray()).ToArray();
+        var nodes = GetNodes(map);
         
         return default;
+    }
+
+    private static Dictionary<char, HashSet<(int x, int y)>> GetNodes(char[][] map)
+    {
+        Dictionary<char, HashSet<(int x, int y)>> nodes = [];
+        for (var y = 0; y < map[0].Length - 1; y++)
+        {
+            for (var x = 0; x < map.Length - 1; x++)
+            {
+                var ch = map[y][x];
+                if (ch == '.') continue;
+                if (!nodes.TryAdd(ch, [])) nodes[ch].Add((x,y));
+            }
+        }
+        
+        return nodes;
     }
 }
