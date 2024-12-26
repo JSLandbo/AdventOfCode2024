f31015f91c8d00f1df28b0a422981957be47304c
JSLandbo <jacob@landbo.dk>
2024-12-26 22:02:31 +0100

    optimize 14 part 2, false if any duplicates.

--- AdventOfCode/Day14.cs
+++ AdventOfCode/Day14.cs
@@ -36,10 +36,25 @@ public sealed class Day14 : BaseDay
         const int width = 101;
         const int height = 103;
         var steps = 0;
-        while (MoveRobots(robots, width, height, ++steps).Any(e => e.Value != 1));
+        while (!MoveRobotsNoDuplicatePositions(robots, width, height, ++steps));
         return new ValueTask<string>($"{steps}");
     }
 
+    private static bool MoveRobotsNoDuplicatePositions(((int x, int y) position, (int x, int y) velocity)[] robots, int width, int height, int steps)
+    {
+        var result = new bool[width, height];
+        foreach (var robot in robots)
+        {
+            var x = (robot.position.x + robot.velocity.x * steps) % width;
+            var y = (robot.position.y + robot.velocity.y * steps) % height;
+            if (x < 0) x += width; 
+            if (y < 0) y += height;
+            if (result[x, y]) return false;
+            result[x, y] = true;
+        }
+        return true;
+    }
+    
     private static Dictionary<(int x, int y), int> MoveRobots(((int x, int y) position, (int x, int y) velocity)[] robots, int width, int height, int steps)
     {
         var result = new Dictionary<(int x, int y), int>();
